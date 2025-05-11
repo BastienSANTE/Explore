f0bfad6bace4362cddf323ad67b218d852005c0a
Bastien SANTE <b.sante@nouvellesimages.xyz>
2025-05-11 23:35:42 +0200

    Reproduced UC Latin alphabet. Added options to generate both proportional & monospace font from the same source (it works)

--- Metafont/testscript.py
+++ Metafont/testscript.py
@@ -1,19 +1,15 @@
 import fontforge
 
-glypharray = [65, 66, 67, 68, 69, 70, 71]
-
-fn = "";
+filename : str = ""
 
 testfont = fontforge.font()
 
 
-
-for g in glypharray:
-    fn = "{}.svg".format(g)
+for g in range(65, 90):
+    filename = "{}.svg".format(g)
     g = testfont.createChar(g)
-    g.importOutlines(fn)
-    g.right_side_bearing = 0;
+    g.importOutlines(filename)
+    g.right_side_bearing = 0
 
 
-
-testfont.save('font.sfd')
+testfont.save('Explore.sfd')
